0ece84b4ff6a477476409512c7fa94a51b69cac8
jmvilla12 <josemanuel.villa@utp.edu.co>
2024-09-18 10:41:51 -0500

    Mode saved in local storage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ import './App.css';
 
 export default function App() {
 
-    const [darkMode, setDarkMode] = React.useState(true);
+    const [darkMode, setDarkMode] = React.useState(() => {
+        const storedPreference = localStorage.getItem('darkMode');
+        if (storedPreference) {
+            return storedPreference === 'true';
+        }
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        return prefersDarkMode;
+    });
+
+    React.useEffect(() => {
+        localStorage.setItem('darkMode', darkMode);
+      }, [darkMode]);
 
     function toggleDarkMode() {
         setDarkMode(prevMode => !prevMode);
